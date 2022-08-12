256a8323699cdefaf1549c812431d0cfa20bba99
Josiah Brown <88345601+JosiahABrown@users.noreply.github.com>
2022-08-12 10:37:42 -0500

    initial commit

---
+++ main.py
@@ -0,0 +1,5 @@
+from selenium import webdriver
+import page
+
+if __name__ == '__main__':
+    pass
---
+++ page.py
@@ -0,0 +1,5 @@
+import locators
+
+class BasePage(object):
+    def __init__(self, driver):
+        self.driver = driver
